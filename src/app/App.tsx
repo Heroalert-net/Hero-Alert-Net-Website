655fdfe7422ac64777dffcbefb62a18695d60bd3
Heroalert-net <elvis@heroalert.net>
2026-06-20 13:53:36 -0400

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ImageWithFallback } from "@/app/components/figma/ImageWithFallback";
 import Home from "./components/pages/Home";
 import logoSrc from "@/imports/HA_Logo_With_Words__1_.jpg";
-import howItWorksBg from "@/imports/ChatGPT_Image_Jun_19__2026__07_59_37_PM.png";
+import howItWorksBg from "@/imports/ChatGPT Image Jun 19, 2026, 07_59_37 PM.png;
 import dispatchCenterImg from "@/imports/ChatGPT_Image_Jun_19__2026__08_27_19_PM.png";
 import aboutBg from "@/imports/ChatGPT_Image_Jun_19__2026__08_35_49_PM.png";
 
